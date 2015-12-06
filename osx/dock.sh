049701fc1225633cdae8ad3e0ef879fc7573524f
Alexander Myshov <simplex.light@gmail.com>
2015-12-06 16:13:58 +0600

    Update dock applications

--- osx/dock.sh
+++ osx/dock.sh
@@ -3,12 +3,14 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
 dockutil --no-restart --add "$HOME/Applications/Firefox.app"
+dockutil --no-restart --add "$HOME/Applications/Google Chrome Canary.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/Applications/iTunes.app"
+dockutil --no-restart --add "$HOME/Applications/Keka.app"
+dockutil --no-restart --add "$HOME/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Utilities/Console.app"
-dockutil --no-restart --add "$HOME/Applications/Spotify.app"
 
 killall Dock
