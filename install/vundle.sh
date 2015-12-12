4f49840184be960a04425eb84ffa17b26fcd4d6d
Alexander Myshov <simplex.light@gmail.com>
2015-12-12 13:10:38 +0600

    Remove vim sessions stuff

--- install/vundle.sh
+++ install/vundle.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 BUNDLE_DIR=~/.vim/bundle
-SESSIONS_DIR=~/.vim/sessions
-
-# Prepare folder for sessions
-mkdir -p "$SESSIONS_DIR"
 
 # Install/update Vundle
 mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/gmarik/vundle.git "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
