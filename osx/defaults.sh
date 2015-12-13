0def9002533486d2fa51b2334d6a3d8b15061921
Alexander Myshov <simplex.light@gmail.com>
2015-12-13 15:55:18 +0600

    Turn off blazingly fast key repeatition

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -115,9 +115,6 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
-
 # Automatically illuminate built-in MacBook keyboard in low light
 defaults write com.apple.BezelServices kDim -bool true
 # Turn off keyboard illumination when computer is not used for 5 minutes
